c7b6da660768fb29ec8db2b985bef83a4b9e8616
Justas Daugintis <justasd101@gmail.com>
2020-10-02 15:07:27 +0300

    product edit component

--- src/components/ProductEdit/ProductEdit.tsx
+++ src/components/ProductEdit/ProductEdit.tsx
@@ -1,6 +1,10 @@
-import React from "react";
-import { Fragment } from "react";
-import { useParams } from "react-router-dom";
+import { PageHeader } from "antd";
+import React, { Fragment } from "react";
+import { useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+import { IRootState } from "../../reducers/CombinedReducer";
+import { IProduct } from "../../sharedInterfaces/IProduct";
+import { ProductForm } from "../ProductForm";
 
 interface ParamTypes {
   id: string;
@@ -10,5 +14,19 @@ interface ParamTypes {
  */
 export const ProductEdit: React.FC = () => {
   let { id } = useParams<ParamTypes>();
-  return <Fragment>Edit: {id}</Fragment>;
+  const { products } = useSelector((state: IRootState) => state.warehouseData);
+  const product: IProduct | undefined = products.find((product: IProduct) => product.id === id);
+  const history = useHistory();
+  return (
+    <Fragment>
+      <PageHeader
+        className="site-page-header"
+        onBack={() => {
+          history.goBack();
+        }}
+        title="Edit product"
+      />
+      <ProductForm productToEdit={product} />
+    </Fragment>
+  );
 };
